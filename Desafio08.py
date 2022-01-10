41e777414f38ee56e1f85e8e822127ce1c85ddf2
Alysson Silva <alyssonsp5@gmail.com>
2022-01-10 17:05:37 -0300

    Desafio08

---
+++ Desafio08.py
@@ -0,0 +1,6 @@
+n1 = int(input('Escreva um valor em metro: '))
+c = n1*100
+m = n1*1000
+print('Você digitou {} m' .format(n1))
+print('Que é o mesmo de {} centimetros.' .format(c))
+print('Que é o mesmo de {} milimetros.' .format(m))
