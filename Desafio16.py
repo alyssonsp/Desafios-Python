f01c194cfbba24636f8dc1e03e1df83e6e2c7525
Alysson Silva <alyssonsp5@gmail.com>
2022-01-11 18:18:37 -0300

    Desafio16

--- Desafio16.py
+++ Desafio16.py
@@ -1,4 +1,4 @@
 import math
 quebrado = float(input("Digite um numero quebrado: "))
 inteiro = math.floor(quebrado)
-print("O nuemro digitado foi {.:2f}, mas sua posição inteira é {.:2f} " .format(quebrado, inteiro))
+print("O numero digitado foi {}, mas sua posição inteira é {} " .format(quebrado, inteiro))
