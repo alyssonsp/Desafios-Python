ef2c3fc5f5c616fe9d268f1649e895f99755f01c
alyssonsp <43155633+alyssonsp@users.noreply.github.com>
2022-01-12 09:47:53 -0300

    Delete Desafio04.py

--- Desafio04.py
+++
@@ -1,8 +0,0 @@
-n = input('Digite algo ')
-print('O tipo primitivo desse valor é: ', type(n))
-print('O que digitou é uma letra ?', n.isalpha())
-print('O que digitou é um número ?', n.isnumeric())
-print('O que digitou é alfanúmerico?', n.isalnum())
-print('O que digitou é uma letra maiúscula ?', n.isupper())
-print('O que digitou é uma letra minúsculal ?', n.islower())
-
