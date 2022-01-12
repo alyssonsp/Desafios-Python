083731fd2cbd7db268878bb224a818bdb03af281
alyssonsp <43155633+alyssonsp@users.noreply.github.com>
2022-01-12 09:49:16 -0300

    Delete Desafio16.py

--- Desafio16.py
+++
@@ -1,4 +0,0 @@
-import math
-quebrado = float(input("Digite um numero quebrado: "))
-inteiro = math.floor(quebrado)
-print("O numero digitado foi {}, mas sua posição inteira é {} " .format(quebrado, inteiro))
