0a48b4b5c1ff80a59e16df3e4ab2d239d2596ff1
Alysson Silva <alyssonsp5@gmail.com>
2022-01-12 09:55:34 -0300

    Desafio

--- Desafio01.py
+++ Desafio01.py
@@ -1,2 +1,4 @@
+# Desafio 01 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
 nome = input('Qual é o seu nome? ')
 print('É um prazer te conhecer {}!' .format(nome))
