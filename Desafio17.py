386eb98fc612630dda272b22f95146f65ae45b66
alyssonsp <43155633+alyssonsp@users.noreply.github.com>
2022-01-12 12:27:48 -0300

    Delete Desafio17.py

--- Desafio17.py
+++
@@ -1,9 +0,0 @@
-# Desafio 17 - Faça um programa que leia o comprimento do cateto
-# oposto e do cateto adjacente de um triângulo retângulo e mostre
-# o comprimendo da hipotenusa.
-
-o = float(input('Comprimento do cateto oposto: '))
-a = float(input('Comprimento do cateto adjacente: '))
-hi = (o ** 2 + a ** 2) ** (1/2)
-print('A hipotenusa vai medir {:.2f}.' .format(hi))
-
