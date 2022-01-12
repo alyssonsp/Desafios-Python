ba1075fd0122cb12f52bb1bbfd71dee91929aa1d
alyssonsp <43155633+alyssonsp@users.noreply.github.com>
2022-01-12 09:48:42 -0300

    Delete Desafio12.py

--- Desafio12.py
+++
@@ -1,3 +0,0 @@
-preco = float(input('Qual o preço do produto? R$'))
-desconto = preco - (preco * 5) /100
-print('O preço que custava R${:.2f}, na promoção com desconto de 5%, irá custar R${:.2f} ' .format(preco, desconto))
